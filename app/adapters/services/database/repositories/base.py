e9cf3040881055d1c0040363954e1168bfb7f297
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-09-18 13:30:17 +0300

    create base service

--- app/adapters/services/database/repositories/base.py
+++ app/adapters/services/database/repositories/base.py
@@ -3,7 +3,7 @@ from typing import ClassVar, Type
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .crud import CRUDRepository, Model
+from .crud import CRUDRepository, CRUDService, UOW, Model
 
 
 class BaseRepository(abc.ABC):
@@ -12,3 +12,8 @@ class BaseRepository(abc.ABC):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
         self._crud = CRUDRepository(session, self.model)
+
+
+class BaseService(abc.ABC):
+    def __init__(self, uow: Type[UOW], repository: str) -> None:
+        self._service = CRUDService(uow, repository)
